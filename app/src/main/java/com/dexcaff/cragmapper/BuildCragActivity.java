e294f1ab9fe7b82253504c3934a8ef8a29efb7df
Dexter Caffery <dcaffery@crimsonagility.com>
2016-12-13 16:02:48 -0500

    Added new action bar support for adding route page
    note - getSupportActionBar() is always used with AppCompat
    TODO: add 'back' functionality when button is hit from
    add route page - right now, back arrow vs native back button
    do two different things (one spawns a new onCreate of the main page)

--- app/src/main/java/com/dexcaff/cragmapper/BuildCragActivity.java
+++ app/src/main/java/com/dexcaff/cragmapper/BuildCragActivity.java
@@ -3,6 +3,9 @@ package com.dexcaff.cragmapper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class BuildCragActivity extends AppCompatActivity {
 
@@ -13,5 +16,39 @@ public class BuildCragActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
 
+        // BEGIN_INCLUDE (inflate_set_custom_view)
+        // Inflate a "Done/Cancel" custom action bar view.
+        final LayoutInflater inflater = (LayoutInflater) getSupportActionBar().getThemedContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View customActionBarView = inflater.inflate(
+                R.layout.entity_save_toolbar, null);
+        customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // "Done"
+                        finish();
+                    }
+                });
+        customActionBarView.findViewById(R.id.actionbar_cancel).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // "Cancel"
+                        finish();
+                    }
+                });
+
+        // Show the custom action bar view and hide the normal Home icon and title.
+        final android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayOptions(
+                android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM,
+                android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM | android.support.v7.app.ActionBar.DISPLAY_SHOW_HOME
+                        | android.support.v7.app.ActionBar.DISPLAY_SHOW_TITLE);
+        actionBar.setCustomView(customActionBarView,
+                new android.support.v7.app.ActionBar.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+        // END_INCLUDE (inflate_set_custom_view)
     }
 }
