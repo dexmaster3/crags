545fe5c3122fda6e0ed7d82f7178feae1b425867
Dexter Caffery <dcaffery@crimsonagility.com>
2016-12-30 15:45:21 -0500

    Added crag image point mapper activity
    will need db handling for point mapping
    need to fix up action bar events

--- app/src/main/java/com/dexcaff/cragmapper/EditCragImageActivity.java
+++ app/src/main/java/com/dexcaff/cragmapper/EditCragImageActivity.java
@@ -1,6 +1,5 @@
 package com.dexcaff.cragmapper;
 
-import android.app.Dialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -12,12 +11,10 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.view.ViewGroup;
 
 import com.dexcaff.cragmapper.db.CragContract;
 import com.dexcaff.cragmapper.libs.TouchImageView;
@@ -29,11 +26,17 @@ import com.dexcaff.cragmapper.models.Crag;
  */
 public class EditCragImageActivity extends AppCompatActivity {
     private TouchImageView mImageView;
+    private Uri mOriginalImage;
+    private android.support.v7.app.ActionBar mActionBar;
+    private int mActionBarOptions;
+    private boolean mTouchActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //todo image resizing etc..
         super.onCreate(savedInstanceState);
+        mActionBar = getSupportActionBar();
+        mActionBarOptions = mActionBar.getDisplayOptions();
         Crag crag = Crag.getCragById(getBaseContext(), getIntent().getLongExtra(Crag.EXTRA_TAG, -1));
 
         setContentView(R.layout.activity_edit_crag_image);
@@ -41,9 +44,9 @@ public class EditCragImageActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-
+        mOriginalImage = Uri.parse((String) crag.properties.get(CragContract.CragEntry.COLUMN_NAME_IMAGE));
         mImageView = (TouchImageView) findViewById(R.id.crag_edit_image_view);
-        mImageView.setImageURI(Uri.parse((String) crag.properties.get(CragContract.CragEntry.COLUMN_NAME_IMAGE)));
+        mImageView.setImageURI(mOriginalImage);
         mImageView.setOnTouchListener(
                 new View.OnTouchListener() {
                     @Override
@@ -73,27 +76,83 @@ public class EditCragImageActivity extends AppCompatActivity {
     private boolean addCragTouch(MotionEvent event) {
         final int action = event.getAction();
         if (action == MotionEvent.ACTION_DOWN) {
-            int x = (int) event.getX();
-            int y = (int) event.getY();
-            //todo should all contexts be replaced with class this?
-            Dialog dialog = new Dialog(EditCragImageActivity.this);
-            dialog.setContentView(R.layout.edit_crag_image_popup);
-            WindowManager.LayoutParams wmlp = dialog.getWindow().getAttributes();
-            wmlp.gravity = Gravity.TOP | Gravity.CENTER;
-            wmlp.x = x;
-            wmlp.y = y;
-            Button btnSave = (Button) dialog.findViewById(R.id.crag_edit_image_point_save);
-            dialog.show();
+            if (!toggleTouchActive()) {
+                showAddCragActionBar();
 
-            Bitmap cragImage = ((BitmapDrawable)mImageView.getDrawable()).getBitmap();
-            Bitmap cragNode = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_cancel);
-            Bitmap resultBitmap = Bitmap.createBitmap(cragImage.getWidth(),cragImage.getHeight(), cragImage.getConfig());
-            Canvas canvas = new Canvas(resultBitmap);
-            canvas.drawBitmap(cragImage, new Matrix(), null);
-            canvas.drawBitmap(cragNode, (cragImage.getWidth() - cragNode.getWidth()) / 2, (cragImage.getHeight() - cragNode.getHeight()) / 2, new Paint());
+                float[] touchCoords = getViewCoords(event, mImageView);
+                Bitmap cragImage = ((BitmapDrawable)mImageView.getDrawable()).getBitmap();
+                Bitmap cragNode = BitmapFactory.decodeResource(getResources(), R.drawable.ic_action_cancel);
+                Bitmap resultBitmap = Bitmap.createBitmap(cragImage.getWidth(),cragImage.getHeight(), cragImage.getConfig());
+                Canvas canvas = new Canvas(resultBitmap);
+                canvas.drawBitmap(cragImage, new Matrix(), null);
+                canvas.drawBitmap(cragNode, touchCoords[0] - (cragNode.getWidth() / 2), touchCoords[1] - (cragNode.getHeight() / 2), new Paint());
 
-            mImageView.setImageBitmap(resultBitmap);
+                mImageView.setImageBitmap(resultBitmap);
+            } else {
+                hideAddCragActionBar();
+                mImageView.setImageURI(mOriginalImage);
+            }
         }
         return false;
+   }
+
+    private boolean toggleTouchActive() {
+        mTouchActive = !mTouchActive;
+        return !mTouchActive;
+    }
+
+    private void hideCragTouch() {
+
+    }
+
+    private void saveCragTouch() {
+
+    }
+
+    private void showAddCragActionBar() {
+        mActionBar.setCustomView(R.layout.entity_save_toolbar);
+        View actionBarView = mActionBar.getCustomView();
+        actionBarView.findViewById(R.id.actionbar_cancel).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                });
+        actionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        saveCragTouch();
+                    }
+                });
+
+        mActionBar.setDisplayOptions(
+                android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM,
+                android.support.v7.app.ActionBar.DISPLAY_SHOW_CUSTOM
+                        | android.support.v7.app.ActionBar.DISPLAY_SHOW_HOME
+                        | android.support.v7.app.ActionBar.DISPLAY_SHOW_TITLE);
+        mActionBar.setCustomView(actionBarView,
+                new android.support.v7.app.ActionBar.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void hideAddCragActionBar() {
+        mActionBar.setDisplayShowCustomEnabled(false);
+        mActionBar.setDisplayOptions(mActionBarOptions);
+    }
+
+    private float[] getViewCoords(MotionEvent event, TouchImageView imageView) {
+        Matrix invertMatrix = new Matrix();
+        imageView.getImageMatrix().invert(invertMatrix);
+        float[] touchCoords = new float[] {event.getX(), event.getY()};
+        invertMatrix.mapPoints(touchCoords);
+        //Todo density could be an issue but this doesn't work
+        float density = getResources().getDisplayMetrics().density;
+        touchCoords[0] *= density;
+        touchCoords[1] *= density;
+        return touchCoords;
     }
 }
