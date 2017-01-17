4ecbf622ae1b1e1aa273aade34a565ad59c55dbb
Dexter Caffery <dexmaster3@gmail.com>
2017-01-16 20:52:52 -0500

    Working point coords / view scaling
    need animation for new point / ordering /
    node saving working

--- app/src/main/java/com/dexcaff/cragmapper/EditCragImageActivity.java
+++ app/src/main/java/com/dexcaff/cragmapper/EditCragImageActivity.java
@@ -1,9 +1,5 @@
 package com.dexcaff.cragmapper;
 
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
@@ -13,17 +9,11 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 import com.dexcaff.cragmapper.db.CragContract;
 import com.dexcaff.cragmapper.libs.EditCragImageView;
-import com.dexcaff.cragmapper.libs.NodeCircle;
-import com.dexcaff.cragmapper.libs.TouchImageView;
 import com.dexcaff.cragmapper.models.Crag;
 import com.dexcaff.cragmapper.models.Node;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -31,14 +21,12 @@ import java.util.Map;
  */
 public class EditCragImageActivity extends AppCompatActivity {
     private final static String TAG = "EditCragImageActivity";
+    private EditCragImageView mContentView;
     private Crag mCurrentCrag;
-    private TouchImageView mImageView;
-    private Uri mOriginalImage;
     private android.support.v7.app.ActionBar mActionBar;
     private int mActionBarOptions;
-    private boolean mTouchActive;
+    private boolean mTouchActive = false;
     private float[] mCurrentTouchCoords;
-    private FrameLayout mFramelay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,23 +40,14 @@ public class EditCragImageActivity extends AppCompatActivity {
         }
 
         mCurrentCrag = Crag.getCragById(getBaseContext(), getIntent().getLongExtra(Crag.EXTRA_TAG, -1));
-        mOriginalImage = Uri.parse((String) mCurrentCrag.properties.get(CragContract.CragEntry.COLUMN_NAME_IMAGE));
-        View contentView = new EditCragImageView(this, mOriginalImage.toString());
-        setContentView(R.layout.activity_edit_crag_image);
-
-        mFramelay = (FrameLayout) findViewById(R.id.edit_img_frame);
-        getPopulatedCragBitmap();
-
-        mImageView = (TouchImageView) findViewById(R.id.crag_edit_image_view);
-        mImageView.setImageDrawable(getPopulatedCragBitmap());
-        mImageView.setOnTouchListener(
-                new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View v, MotionEvent event) {
-                        return addCragTouch(event);
-                    }
-                }
-        );
+        mContentView = new EditCragImageView(this, mCurrentCrag);
+        mContentView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return addCragTouch(event);
+            }
+        });
+        setContentView(mContentView);
     }
 
     @Override
@@ -90,52 +69,47 @@ public class EditCragImageActivity extends AppCompatActivity {
     private boolean addCragTouch(MotionEvent event) {
         final int action = event.getAction();
         if (action == MotionEvent.ACTION_DOWN) {
-            if (!toggleTouchActive()) {
-                getViewCoords(event, mImageView);
-//                Bitmap cragImage = getPopulatedCragBitmap();
-//                Bitmap cragNode = BitmapFactory.decodeResource(getResources(), R.drawable.node_circle);
-//                Bitmap resultBitmap = Bitmap.createBitmap(cragImage.getWidth(),cragImage.getHeight(), cragImage.getConfig());
-//                Canvas canvas = new Canvas(resultBitmap);
-//                canvas.drawBitmap(cragImage, new Matrix(), null);
-//                NodeCircle testr = new NodeCircle(getApplicationContext(), null);
-//                testr.draw(canvas);
-//                canvas.drawBitmap(cragNode, mCurrentTouchCoords[0] - (cragNode.getWidth() / 2), mCurrentTouchCoords[1] - (cragNode.getHeight() / 2), new Paint());
-
-//                mImageView.setImageBitmap(resultBitmap);
-            } else {
-                mImageView.setImageDrawable(getPopulatedCragBitmap());
+            if (!mTouchActive && mContentView.drawTempNode(event)) {
+                showAddCragActionBar();
             }
         }
         return false;
    }
 
-    private void saveCragTouch() {
+    private void saveCragButton() {
         try {
             long cragid = (long) mCurrentCrag.properties.get(CragContract.CragEntry._ID);
             Node node = new Node(-1, cragid, mCurrentTouchCoords[0], mCurrentTouchCoords[1]);
             node.addNode(getBaseContext());
-            toggleTouchActive();
-            mImageView.setImageDrawable(getPopulatedCragBitmap());
+            hideAddCragActionBar();
         } catch (Exception ex) {
             Log.d(TAG, "Save node click failed", ex);
         }
     }
 
+    private void cancelButton() {
+        mContentView.removeTempNode();
+        hideAddCragActionBar();
+    }
+
     private void showAddCragActionBar() {
+        if (mTouchActive) {
+            return;
+        }
         mActionBar.setCustomView(R.layout.entity_save_toolbar);
         View actionBarView = mActionBar.getCustomView();
         actionBarView.findViewById(R.id.actionbar_cancel).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        finish();
+                        cancelButton();
                     }
                 });
         actionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        saveCragTouch();
+                        saveCragButton();
                     }
                 });
 
@@ -149,53 +123,15 @@ public class EditCragImageActivity extends AppCompatActivity {
                         ViewGroup.LayoutParams.MATCH_PARENT,
                         ViewGroup.LayoutParams.MATCH_PARENT));
         mActionBar.setDisplayHomeAsUpEnabled(true);
+        mTouchActive = true;
     }
 
     private void hideAddCragActionBar() {
+        if (!mTouchActive) {
+            return;
+        }
         mActionBar.setDisplayShowCustomEnabled(false);
         mActionBar.setDisplayOptions(mActionBarOptions);
-    }
-
-    private BitmapDrawable getPopulatedCragBitmap() {
-        long cragId = (long) mCurrentCrag.properties.get(CragContract.CragEntry._ID);
-        HashMap<String, Node> nodes = Node.getAllNodesByCragId(this, cragId);
-        try {
-            EditCragImageView cragImageView = new EditCragImageView(this, mOriginalImage.toString());
-            for (Map.Entry<String, Node> entry : nodes.entrySet()){
-                Node node = entry.getValue();
-                NodeCircle ragy = new NodeCircle(this, null, node);
-
-                mFramelay.addView(ragy);
-//                ragy.draw(canvas);
-                break;
-            }
-            return new BitmapDrawable(getResources(), Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_4444));
-        } catch (Exception ex) {
-            Log.d(TAG, "Canvas population error", ex);
-            return new BitmapDrawable(getResources(), Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_4444));
-        }
-    }
-
-    private void getViewCoords(MotionEvent event, TouchImageView imageView) {
-        Matrix invertMatrix = new Matrix();
-        imageView.getImageMatrix().invert(invertMatrix);
-        mCurrentTouchCoords = new float[] {event.getX(), event.getY()};
-        invertMatrix.mapPoints(mCurrentTouchCoords);
-        //This should fix density issues
-        float scaleX = imageView.getScaleX();
-        float scaleY = imageView.getScaleY();
-        mCurrentTouchCoords[0] *= scaleX;
-        mCurrentTouchCoords[1] *= scaleY;
-
-    }
-
-    private boolean toggleTouchActive() {
-        if (mTouchActive) {
-            hideAddCragActionBar();
-        } else {
-            showAddCragActionBar();
-        }
-        mTouchActive = !mTouchActive;
-        return !mTouchActive;
+        mTouchActive = false;
     }
 }
--- app/src/main/java/com/dexcaff/cragmapper/libs/EditCragImageView.java
+++ app/src/main/java/com/dexcaff/cragmapper/libs/EditCragImageView.java
@@ -2,7 +2,26 @@ package com.dexcaff.cragmapper.libs;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dexcaff.cragmapper.db.CragContract;
+import com.dexcaff.cragmapper.db.NodeContract;
+import com.dexcaff.cragmapper.models.Crag;
+import com.dexcaff.cragmapper.models.Node;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Dexter <code@dexcaff.com>
@@ -11,27 +30,129 @@ import android.graphics.drawable.BitmapDrawable;
  *          Package: com.dexcaff.cragmapper.libs
  */
 
-public class EditCragImageView extends TouchImageView {
-    //ToDo try adding cust view nodes, onto the xml layout
+public class EditCragImageView extends View {
+    private final static String TAG = "EditCragImageView";
+    private Crag mCurrentCrag;
+    private Node mTempNode;
+    private float mScale, mStartW, mStartH;
+
+    private Rect mContentRect = new Rect();
     private BitmapDrawable mBackground;
+    private ShapeDrawable mDrawableOval = new ShapeDrawable(new OvalShape());
+    private Paint mNodePaint = new Paint();
+    private ShapeDrawable mNodeShape = new ShapeDrawable(new OvalShape());
+    private RectF mNodeRectF = new RectF();
+    private Rect mNodeRect = new Rect();
 
     public EditCragImageView(Context context) {
-        super(context);
+        this(context, new Crag(0, "", "", 0f));
     }
 
     @TargetApi(16)
-    public EditCragImageView(Context context, String backgroundFile) {
+    public EditCragImageView(Context context, Crag currentCrag) {
         super(context);
-        mBackground = new BitmapDrawable(getResources(), backgroundFile);
-        int sdk = android.os.Build.VERSION.SDK_INT;
-        if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
-            setBackgroundDrawable(mBackground);
+        mCurrentCrag = currentCrag;
+        String originalImage = (String) mCurrentCrag.properties.get(CragContract.CragEntry.COLUMN_NAME_IMAGE);
+        setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        mBackground = new BitmapDrawable(getResources(), originalImage);
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mBackground.setBounds(mContentRect);
+        mBackground.draw(canvas);
+
+        drawNodes(canvas);
+
+        mDrawableOval.setBounds(10, 50, canvas.getWidth() / 4, canvas.getHeight() / 8);
+        mDrawableOval.getPaint().setColor(Color.BLUE);
+        mDrawableOval.setAlpha(150);
+        mDrawableOval.draw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Rect imageRect = new Rect();
+        getScaledImageRect().round(imageRect);
+        mContentRect.set(imageRect);
+    }
+
+    private RectF getScaledImageRect() {
+        Bitmap bmp = mBackground.getBitmap();
+        mStartW = getPaddingLeft();
+        mStartH = getPaddingTop();
+        float bmpH = bmp.getHeight();
+        float bmpW = bmp.getWidth();
+        float ratio = bmpW / bmpH;
+        float viewH = getHeight() - (getPaddingTop() + getPaddingBottom());
+        float viewW = getWidth() - (getPaddingLeft() + getPaddingRight());
+        float viewR = viewW / viewH;
+        if (viewR > ratio) {
+            //Viewport is wider -> scale image by max height
+            mScale = viewH / bmpH;
+            viewW = mScale * bmpW;
+            mStartW = (getWidth() - viewW) / 2;
         } else {
-            setBackground(mBackground);
+            //Viewport is taller
+            mScale = viewW / bmpW;
+            viewH = mScale * bmpH;
+            mStartH = (getHeight() - viewH) / 2;
+        }
+        return new RectF(mStartW, mStartH, viewW + mStartW, viewH + mStartH);
+    }
+
+    private void drawNodes(Canvas canvas) {
+        if (mTempNode != null) {
+            drawNode(canvas, mTempNode);
+        }
+        HashMap<String, Node> nodes = (HashMap<String, Node>) mCurrentCrag.properties.get(NodeContract.NodeEntry.TABLE_NAME);
+        for (Map.Entry<String, Node> entry : nodes.entrySet()){
+            Node node = entry.getValue();
+            drawNode(canvas, node);
         }
     }
 
-    public void addNode(NodeCircle node) {
+    private void drawNode(Canvas canvas, Node node) {
+        float[] coords = getScaledNodeCoords(node);
+        mNodeRectF.set(coords[0] - 20, coords[1] - 20, coords[0] + 20, coords[1] + 20);
+        mNodeRectF.round(mNodeRect);
+        mNodeShape.setBounds(mNodeRect);
+        mNodePaint.setColor(Color.MAGENTA);
+        mNodeShape.getPaint().set(mNodePaint);
+        mNodeShape.draw(canvas);
+    }
 
+    public boolean drawTempNode(MotionEvent event) {
+        float xcoord = event.getX();
+        float ycoord = event.getY();
+        xcoord = (xcoord - mStartW) / mScale;
+        ycoord = (ycoord - mStartH) / mScale;
+        if (xcoord < 0 || ycoord < 0 || event.getX() > mContentRect.right || event.getY() > mContentRect.bottom) {
+            mTempNode = null;
+            return false;
+        } else {
+            mTempNode = new Node(-1, (long) mCurrentCrag.properties.get(CragContract.CragEntry._ID), xcoord, ycoord);
+        }
+        invalidate();
+        return true;
+    }
+
+    public void removeTempNode() {
+        mTempNode = null;
+        invalidate();
+    }
+
+    private float[] getScaledNodeCoords(Node node) {
+        float[] coords = new float[2];
+        coords[0] = (float) node.properties.get(NodeContract.NodeEntry.COLUMN_NAME_X_COORD);
+        coords[1] = (float) node.properties.get(NodeContract.NodeEntry.COLUMN_NAME_Y_COORD);
+        coords[0] *= mScale;
+        coords[1] *= mScale;
+        coords[0] += mContentRect.left;
+        coords[1] += mContentRect.top;
+        return coords;
     }
 }
